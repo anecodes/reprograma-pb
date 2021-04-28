75edfbfc448b616f36f700fb5de15a674347ed7e
Juliane <julianelipires@outlook.com>
2021-04-28 15:35:27 -0300

    Arrumando layout da página web e configurando validação em js

---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto front-end do bootcamp RePrograma PB do Paraná Banco 👩‍💻
+
+Projeto desenvolvido durante a semana do bootcamp RePrograma PB, na trilha de front-end. O layout do projeto pode ser acessado [aqui](https://www.figma.com/file/pzyVylrT5t6IYHbBaxU64o/RePrograma-Trilha-Front-end?node-id=0%3A394) e o projeto inicial foi clonado [deste repositório](https://github.com/annelesinhovski/reprograma-inicial).
+
+# Tecnologias utilizadas:
+
+- HTML
+- CSS
+- JavaScript
+- Figma
+
+### © Copyright Paraná Banco 2021
---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+/* Função de validação do cadastro de email */
+
+document.getElementById("botao").addEventListener("click", validaContato);
+
+function validaContato() {
+  let nome = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+
+  /* Validação de formato do email com regex */
+  function validateEmail(email) {
+    var re = new RegExp(
+      /^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/
+    );
+    return re.test(email);
+  }
+
+  if (nome == "") {
+    alert("Qual é o seu nome?");
+    document.getElementById("username").focus();
+    return;
+  }
+  if (email == "" || !validateEmail(email)) {
+    alert("Informe seu email para saber das novidades! ;)");
+    document.getElementById("email").focus();
+    return false;
+  } else {
+    alert("Email cadastrado!");
+  }
+}
